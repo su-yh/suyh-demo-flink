c4964feaf1aaa81272345e33e0b1de19425f51d6
suyh <suyunhong@ater-info.com>
2023-12-25 15:16:34 +0800

    flink 集成spring boot

--- demo-01/d05-springboot/src/main/java/com/suyh/d05/WordCountSprintBootDemo.java
+++ demo-01/d05-springboot/src/main/java/com/suyh/d05/WordCountSprintBootDemo.java
@@ -1,11 +1,15 @@
 package com.suyh.d05;
 
+import com.suyh.d05.bootmain.FlinkBootMain;
+import com.suyh.d05.bootmain.config.properties.FlinkSpringBootProperties;
 import com.suyh.d05.task.RichFlatMap;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @author suyh
@@ -21,8 +25,17 @@ public class WordCountSprintBootDemo {
         // 然后就可以在本地使用 http://localhost:8081 进行访问 了。
         // StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
 
+        ConfigurableApplicationContext context = SpringApplication.run(FlinkBootMain.class, args);
+        FlinkSpringBootProperties properties = context.getBean(FlinkSpringBootProperties.class);
+        log.info("suyh.flink.parallelism: {}", properties.getParallelism());
+
         log.info("suyh - main begin...");
-        // env.setParallelism(1);
+        if (properties.getParallelism() != null) {
+            env.setParallelism(properties.getParallelism());
+        }
+
+        // 因为两个spring boot web 会使用同一个端口，所以这里直接将jobmanager 里面的context 关闭掉。
+        context.close();
 
         // 利用netcat 监听7777 端口： nc -lk 7777
         DataStreamSource<String> socketDS = env.socketTextStream("hadoop001", 8888);
---
+++ demo-01/d05-springboot/src/main/java/com/suyh/d05/bootmain/FlinkBootMain.java
@@ -0,0 +1,11 @@
+package com.suyh.d05.bootmain;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author suyh
+ * @since 2023-12-25
+ */
+@SpringBootApplication
+public class FlinkBootMain {
+}
