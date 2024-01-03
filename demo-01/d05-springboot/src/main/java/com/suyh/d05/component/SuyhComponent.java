d2849966d99018a6e5a987a557be9b14eae50a9b
suyh <suyunhong@ater-info.com>
2024-01-03 12:12:56 +0800

    doc

--- demo-01/d05-springboot/src/main/java/com/suyh/d05/component/SuyhComponent.java
+++ demo-01/d05-springboot/src/main/java/com/suyh/d05/component/SuyhComponent.java
@@ -1,5 +1,7 @@
 package com.suyh.d05.component;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import javax.annotation.PostConstruct;
 
 /**
@@ -8,9 +10,13 @@ import javax.annotation.PostConstruct;
  */
 public class SuyhComponent {
 
+    @Value("${log4j.configuration:}")
+    private String log4jConfigFile;
+
     @PostConstruct
     public void init() {
         System.out.println("suyhComponent init");
+        System.out.println("log4jConfigFile: " + log4jConfigFile);
     }
 
     public void showHello() {
