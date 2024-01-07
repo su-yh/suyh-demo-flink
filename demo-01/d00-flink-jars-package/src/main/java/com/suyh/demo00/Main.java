b809d3ce78f0d75a048b1a0f6ad38244917e6cc9
suyh <su787910081@163.com>
2024-01-07 15:08:50 +0800

    package

---
+++ demo-01/d00-flink-jars-package/src/main/java/com/suyh/demo00/Main.java
@@ -0,0 +1,11 @@
+package com.suyh.demo00;
+
+/**
+ * @author suyh
+ * @since 2024-01-07
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("nothing");
+    }
+}
