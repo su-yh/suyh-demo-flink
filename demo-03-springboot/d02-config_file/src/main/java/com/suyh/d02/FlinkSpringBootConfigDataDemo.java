3583f3640b120e35b66d68558a3fc6d1230c0931
suyh <su787910081@163.com>
2024-01-14 12:46:43 +0800

    flink springboot 外接flink-conf.yaml 配置中的配置项。

--- demo-03-springboot/d02-config_file/src/main/java/com/suyh/d02/FlinkSpringBootConfigDataDemo.java
+++ demo-03-springboot/d02-config_file/src/main/java/com/suyh/d02/FlinkSpringBootConfigDataDemo.java
@@ -1,20 +1,16 @@
 package com.suyh.d02;
 
-import com.suyh.d02.flink.constants.FlinkSpringbootConfigConstants;
 import com.suyh.d02.flink.func.FlinkSpringBootInitFilter;
-import com.suyh.d02.flink.springboot.FlinkSpringbootConfigProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.connector.source.util.ratelimit.RateLimiterStrategy;
-import org.apache.flink.configuration.ReadableConfig;
 import org.apache.flink.connector.datagen.source.DataGeneratorSource;
 import org.apache.flink.connector.datagen.source.GeneratorFunction;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -25,11 +21,6 @@ import java.util.UUID;
 public class FlinkSpringBootConfigDataDemo {
     public static void main(String[] args) {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        ReadableConfig configuration = env.getConfiguration();
-
-        // 初始化springboot 的外置配置
-        String yamlPath = configuration.get(FlinkSpringbootConfigConstants.FLINK_SPRINGBOOT_CONFIG_FILE);
-        Map<String, Object> configProperties = FlinkSpringbootConfigProperties.parse(yamlPath);
 
         DataGeneratorSource<String> dataGeneratorSource = new DataGeneratorSource<>(
                 new GeneratorFunction<Long, String>() {
@@ -45,7 +36,7 @@ public class FlinkSpringBootConfigDataDemo {
 
         DataStreamSource<String> uuidSource
                 = env.fromSource(dataGeneratorSource, WatermarkStrategy.noWatermarks(), "data-generator");
-        SingleOutputStreamOperator<String> mapSource = uuidSource.filter(new FlinkSpringBootInitFilter<>(configProperties));
+        SingleOutputStreamOperator<String> mapSource = uuidSource.filter(new FlinkSpringBootInitFilter<>());
         mapSource.print();
 
         try {
--- demo-03-springboot/d02-config_file/src/main/java/com/suyh/d02/flink/func/FlinkSpringBootInitFilter.java
+++ demo-03-springboot/d02-config_file/src/main/java/com/suyh/d02/flink/func/FlinkSpringBootInitFilter.java
@@ -4,10 +4,12 @@ import com.suyh.d02.flink.vo.FlinkUserTestEntity;
 import com.suyh.d02.property.context.FlinkSpringContext;
 import com.suyh.d02.property.util.JsonUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.functions.RichFilterFunction;
 import org.apache.flink.configuration.Configuration;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -20,11 +22,7 @@ import java.util.Map;
 public class FlinkSpringBootInitFilter<T> extends RichFilterFunction<T> {
     private static final long serialVersionUID = -336842266457209033L;
 
-    private final Map<String, Object> configProperties;
-
-    public FlinkSpringBootInitFilter(Map<String, Object> configProperties) {
-        this.configProperties = configProperties;
-
+    public FlinkSpringBootInitFilter() {
         // TODO: suyh - 不能在这里进行springboot 的初始化，因为它的实例化也是在 flink client 做的
         //  然后对象会被传输到 task manager
     }
@@ -33,12 +31,23 @@ public class FlinkSpringBootInitFilter<T> extends RichFilterFunction<T> {
     public void open(Configuration parameters) throws Exception {
         super.open(parameters);
 
+        ExecutionConfig executionConfig = getRuntimeContext().getExecutionConfig();
+        Map<String, String> sourceMap = executionConfig.toConfiguration().toMap();
+        Map<String, Object> objMap = new HashMap<>();
+        sourceMap.forEach((k, v) -> {
+            log.info("config, key: {}, value: {}", k, v);
+            objMap.put(k, v);
+        });
+//        ExecutionConfig.GlobalJobParameters globalJobParameters = executionConfig.getGlobalJobParameters();
+
+
         log.info("spring boot init: " + FlinkSpringBootInitFilter.class.getSimpleName());
 
         // TODO: suyh - 还没找到TaskManager 只做一次初始化的地方
         //  因为在这里调用的话，每一个并行度都会调用一次。
-        FlinkSpringContext.init(new String[0], configProperties);
+        FlinkSpringContext.init(new String[0], objMap);
 
+        // TODO: suyh - 这些是测试代码
         FlinkUserTestEntity flinkUser = new FlinkUserTestEntity();
         flinkUser.setId(1L).setAge(18).setEmail("su787910081@163.com").setCreateDate(new Date());
 
