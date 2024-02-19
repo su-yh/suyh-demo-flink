4d6c6f05879115dc438d7d76ed0c360a276ca9e3
suyh <suyunhong@ater-info.com>
2024-02-19 15:39:33 +0800

    trigger 窗口触发器，直接使用TimeWindow 即可。

--- demo-04-debug/d03-flink_trigger/src/main/java/com/suyh/d03/SuyhContinuousProcessingTimeTrigger.java
+++ demo-04-debug/d03-flink_trigger/src/main/java/com/suyh/d03/SuyhContinuousProcessingTimeTrigger.java
@@ -3,13 +3,13 @@ package com.suyh.d03;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.triggers.Trigger;
 import org.apache.flink.streaming.api.windowing.triggers.TriggerResult;
-import org.apache.flink.streaming.api.windowing.windows.Window;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 
 /**
  * @author suyh
  * @since 2024-01-31
  */
-public class SuyhContinuousProcessingTimeTrigger<W extends Window> extends Trigger<Object, W> {
+public class SuyhContinuousProcessingTimeTrigger extends Trigger<Object, TimeWindow> {
     private static final long serialVersionUID = 1L;
 
     private final long interval;
@@ -21,7 +21,7 @@ public class SuyhContinuousProcessingTimeTrigger<W extends Window> extends Trigg
     }
 
     @Override
-    public TriggerResult onElement(Object element, long timestamp, W window, TriggerContext ctx)
+    public TriggerResult onElement(Object element, long timestamp, TimeWindow window, TriggerContext ctx)
             throws Exception {
         // 虽然这里的触发器逻辑跟元素没有任何关系，但是这个触发器没有提供初始化方法，所以也就只能在这里进行初始化操作了。
         if (!init) {
@@ -32,7 +32,7 @@ public class SuyhContinuousProcessingTimeTrigger<W extends Window> extends Trigg
     }
 
     @Override
-    public TriggerResult onEventTime(long time, W window, TriggerContext ctx) throws Exception {
+    public TriggerResult onEventTime(long time, TimeWindow window, TriggerContext ctx) throws Exception {
         return TriggerResult.CONTINUE;
     }
 
@@ -44,7 +44,7 @@ public class SuyhContinuousProcessingTimeTrigger<W extends Window> extends Trigg
      * 2. 触发当前窗口计算
      */
     @Override
-    public TriggerResult onProcessingTime(long time, W window, TriggerContext ctx)
+    public TriggerResult onProcessingTime(long time, TimeWindow window, TriggerContext ctx)
             throws Exception {
         // 注册的定时器到了的实现逻辑
         // 注册下一个定时器
@@ -55,7 +55,7 @@ public class SuyhContinuousProcessingTimeTrigger<W extends Window> extends Trigg
     }
 
     @Override
-    public void clear(W window, TriggerContext ctx) throws Exception {
+    public void clear(TimeWindow window, TriggerContext ctx) throws Exception {
         if (timerTimestamp == null) {
             return;
         }
@@ -65,8 +65,8 @@ public class SuyhContinuousProcessingTimeTrigger<W extends Window> extends Trigg
         timerTimestamp = null;
     }
 
-    public static <W extends Window> SuyhContinuousProcessingTimeTrigger<W> of(Time interval) {
-        return new SuyhContinuousProcessingTimeTrigger<>(interval.toMilliseconds());
+    public static SuyhContinuousProcessingTimeTrigger of(Time interval) {
+        return new SuyhContinuousProcessingTimeTrigger(interval.toMilliseconds());
     }
 
     private void registerNextFireTimestamp(TriggerContext ctx) {
